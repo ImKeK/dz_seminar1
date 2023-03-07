523b7257cc76efc255e2b80e6a85aa318380c073
*Egor <egorkiper21@gmail.com>
2023-03-07 21:29:45 +0300

    Решена вторая задача

--- Program.cs
+++ Program.cs
@@ -12,3 +12,51 @@
 //   Console.Write($"Большее число - {b}");
 // }
 
+// Console.WriteLine("Введите три числа: ");
+
+// int num1 = int.Parse(Console.ReadLine()!);
+// int num2 = int.Parse(Console.ReadLine()!);
+// int num3 = int.Parse(Console.ReadLine()!);
+
+// int max = num1;
+
+// if(max > num2)
+// {
+//   max = num1;
+// }
+// else
+// {
+//   max = num2;
+// }
+
+// if(max > num3)
+// {
+//   max = num1;               --- ЗАДАЧА 2
+// }
+// else
+// {
+//   max = num3;
+// }
+
+// if (num1 > num2)                     
+// {
+//   max = num1;
+// }
+
+// else 
+// {
+//   max = num2;
+// }
+
+// if (num2 > num3)
+// {
+//   max = num2;
+// }
+
+// else
+// {
+//   max = num3;
+// }
+// Console.Write($"Большее число - {max}");
+
+
