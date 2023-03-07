644cb7205c7fae58b66eed13051558a5103b541f
*Egor <egorkiper21@gmail.com>
2023-03-07 21:20:24 +0300

    Решена первая задача

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите два числа: ");
+// Console.WriteLine("Введите два числа: ");
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+
+// if (a>b) 
+// { 
+//   Console.Write($"Большее число - {a}");   --- ЗАДАЧА 1
+// }
+// else
+// {
+//   Console.Write($"Большее число - {b}");
+// }
 
